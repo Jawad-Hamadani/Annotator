6de11c9644ef23f8dd5f0aa81571d10cba25e659
Jawad-Hamadani <jawad_h_h@hotmail.com>
2021-10-16 11:03:15 +0300

    commit

--- src/components/utilities/SearchBar.js
+++ src/components/utilities/SearchBar.js
@@ -1,15 +1,36 @@
-import { Button } from '@material-ui/core';
-import { TextField } from '@material-ui/core';
+// MIT License
+//
+// Copyright 2021 Mohammad Jawad Moshati Hamadani
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+import { Button } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
 
 const SearchBar = ({ button, label }) => {
   return (
     <div>
-      <TextField id='outlined-basic' label={label} variant='outlined' />
+      <TextField id="outlined-basic" label={label} variant="outlined" />
       <Button
-        size='large'
-        style={{ marginTop: '10px' }}
-        variant='contained'
-        color='primary'
+        size="large"
+        style={{ marginTop: "10px" }}
+        variant="contained"
+        color="primary"
       >
         {button}
       </Button>
--- src/components/utilities/SelectGroupSelection2.js
+++ src/components/utilities/SelectGroupSelection2.js
@@ -1,35 +1,56 @@
-import { Fragment } from 'react';
-import SelectGroup from './SelectGroup';
+// MIT License
+//
+// Copyright 2021 Mohammad Jawad Moshati Hamadani
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+import { Fragment } from "react";
+import SelectGroup from "./SelectGroup";
 
 const SelectGroupSelection2 = () => {
   return (
     <Fragment>
-      <div className='flex-row'>
+      <div className="flex-row">
         <SelectGroup
-          spacing='1'
-          formWidth='100px'
-          options={['Raw', 'CODA', 'Segment']}
-          id='Select-1'
+          spacing="1"
+          formWidth="100px"
+          options={["Raw", "CODA", "Segment"]}
+          id="Select-1"
         />
         <SelectGroup
-          spacing='1'
-          formWidth='100px'
-          options={['Text', 'Verb Form', 'POS', 'Lemma']}
-          id='Select-2'
+          spacing="1"
+          formWidth="100px"
+          options={["Text", "Verb Form", "POS", "Lemma"]}
+          id="Select-2"
         />
       </div>
-      <div className='flex-row'>
+      <div className="flex-row">
         <SelectGroup
-          spacing='1'
-          formWidth='100px'
-          options={['Approximate', 'Exact']}
-          id='Select-3'
+          spacing="1"
+          formWidth="100px"
+          options={["Approximate", "Exact"]}
+          id="Select-3"
         />
         <SelectGroup
-          spacing='1'
-          formWidth='100px'
-          options={['Me', 'Wiaam', 'Sara', 'All', 'All But Me']}
-          id='Select-4'
+          spacing="1"
+          formWidth="100px"
+          options={["Me", "Wiaam", "Sara", "All", "All But Me"]}
+          id="Select-4"
         />
       </div>
     </Fragment>
